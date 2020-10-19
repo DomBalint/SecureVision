3bf52bee7c1a71d09d3ae7ae747075320c243ff8
Balint Domian <bu1t3e@inf.elte.hu>
2020-10-19 21:55:07 +0200

    ADD: Object model and .sql file

---
+++ source/backend/database/database_model.py
@@ -0,0 +1,78 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Sequence
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(100))
+    password = Column(String(50))
+    user_rights = Column(Integer)
+
+    def __repr__(self):
+        return "<User(name='%s', user_rights='%d')>" % (self.name, self.user_rights)
+
+
+class Camera(Base):
+    __tablename__ = 'camera'
+    id = Column(Integer, Sequence('camera_id_seq'), primary_key=True)
+    is_running = Column(Boolean)
+
+    def __repr__(self):
+        return "<Camera(name='%d', is_running='%d')>" % (self.id, self.is_running)
+
+
+class Image(Base):
+    __tablename__ = 'image'
+    id = Column(Integer, Sequence('image_id_seq'), primary_key=True)
+    img_path = Column(String(200))
+    cam_id = Column(Integer, ForeignKey('camera.id'))
+    cam = relationship("Camera", back_populates="camera")
+
+    def __repr__(self):
+        return "<Image(id='%d', img_path='%s')>" % (self.id, self.img_path)
+
+
+class Detection(Base):
+    __tablename__ = 'detection'
+    id = Column(Integer, Sequence('detection_id_seq'), primary_key=True)
+    img_id = Column(Integer, ForeignKey('image.id'))
+    image = relationship("Image", back_populates="detection")
+
+    def __repr__(self):
+        return "<Detection(id='%d', img_id='%d')>" % (self.id, self.img_id)
+
+
+class Annotation(Base):
+    __tablename__ = 'annotation'
+    id = Column(Integer, Sequence('anno_id_seq'), primary_key=True)
+    obj_type = Column(String(100))
+    left_x = Column(Float)
+    left_y = Column(Float)
+    length = Column(Float)
+    width = Column(Float)
+    detection_id = Column(Integer, ForeignKey('detection.id'))
+    detection = relationship("Detection", back_populates="annotation")
+
+    def __repr__(self):
+        return "<Annotation(id='%s', obj_type='%s')>" % (self.id, self.obj_type)
+
+
+class Feedback(Base):
+    __tablename__ = 'feedback'
+    id = Column(Integer, Sequence('feedback_id_seq'), primary_key=True)
+    corr_obj_type = Column(String(100))
+    corr_left_x = Column(Float)
+    corr_left_y = Column(Float)
+    corr_length = Column(Float)
+    corr_width = Column(Float)
+    detection_id = Column(Integer, ForeignKey('detection.id'))
+    detection = relationship("Detection", back_populates="annotation")
+
+    def __repr__(self):
+        return "<Feedback(id='%s', obj_type='%s')>" % (self.id, self.corr_obj_type)
---
+++ source/backend/database/securevision_schema.sql
@@ -0,0 +1,83 @@
+DROP DATABASE IF EXISTS SecureVision;
+CREATE DATABASE SecureVision;
+
+USE SecureVision;
+
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Camera;
+DROP TABLE IF EXISTS Image;
+DROP TABLE IF EXISTS Detection;
+DROP TABLE IF EXISTS Annotation;
+DROP TABLE IF EXISTS Feedback;
+
+CREATE TABLE User
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    user_pass varchar(255) NOT NULL,
+    user_rights int NOT NULL,
+    num_feedback int,
+
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE Cameras
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    is_running boolean NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE Image
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    img_path varchar(255) NOT NULL,
+    cam_id bigint NOT NULL,
+
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (cam_id) REFERENCES Cameras (id)
+
+);
+
+
+CREATE TABLE Detection
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    img_id bigint NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (img_id) REFERENCES Image (id),
+
+);
+
+
+CREATE TABLE Annotation
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    type varchar(255) NOT NULL,
+    left_x double NOT NULL,
+    left_y double NOT NULL,
+    length double NOT NULL,
+    width double NOT NULL,
+    det_id bigint NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (det_id) REFERENCES Detection (id)
+);
+
+CREATE TABLE Feedback
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    corr_type varchar(255) NOT NULL,
+    corr_left_x double NOT NULL,
+    corr_left_y double NOT NULL,
+    corr_length double NOT NULL,
+    corr_width double NOT NULL,
+    det_id bigint NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (det_id) REFERENCES Detection (id)
+);
