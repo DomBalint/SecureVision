8dd866246a15721b8f97bccbb84019059127e00c
ttofi <z2rciu@inf.elte.hu>
2020-10-21 15:48:20 +0200

    Created and connected DB (now for sure)

--- source/backend/database/base.py
+++ source/backend/database/base.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # Creates a local sqlite database to the database folder
 # TODO: Reconsider database type, can be MySQL or MariaDB
 # TODO: Migrate to remote database so it can be reached by everyone
-engine = create_engine('sqlite:///secure_vision.db', echo=True)
+DATABASE_URL = 'postgres://hcxizfripeejzp:b8842bc09501d56dbeae98802c19f8d4fc9f51557bcf1c9f8588614c4d17f7de@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/d8s5i6sv77e5kc'
+engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(bind=engine)
-
 Base = declarative_base()
