32eca358226f7dea51af7e82a6c9e547fb084b19
Balint Domian <bu1t3e@inf.elte.hu>
2020-11-10 15:55:43 +0100

    Clean test_apis

--- source/backend/tests/test_apis.py
+++ source/backend/tests/test_apis.py
@@ -1,8 +1,6 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-# BASE = 'http://127.0.0.1:5000/'
-
 
 class TestLoginApi:
 
@@ -16,56 +14,3 @@ class TestLoginApi:
         # Verify
         assert response.status_code == desired
         # Cleanup
-
-# Try login: bad username
-# response = requests.get(BASE + 'user/login', auth=HTTPBasicAuth('Guard10', 'pass'))
-# print("401 expected:")
-# print(response)
-# print('')
-
-# # Try login: bad password
-# response = requests.get(BASE + 'user/login', auth=HTTPBasicAuth('Guard1', 'bad_password'))
-# print("401 expected:")
-# print(response)
-# print('')
-#
-# # Try login:  good username and password (with admin)
-# response = requests.get(BASE + 'user/login', auth=HTTPBasicAuth('Guard1', 'Guard1pass'))
-# mock1_token = response.json()['token']
-# print('Valid token expected:')
-# print(response.json())
-# print('')
-
-# # Try login:  good username and password (without admin)
-# response = requests.get(BASE + 'user/login', auth=HTTPBasicAuth('Guard3', 'Guard3pass'))
-# mock2_token = response.json()['token']
-# print('Valid token expected:')
-# print(response.json())
-# print('')
-#
-# # Try admin function: with NOT an admin user
-# header = {'x-access-token': mock2_token}
-# response = requests.put(BASE + 'user/6666666', headers=header)
-# print('Cannot perform expected:')
-# print(response.json())
-# print('')
-
-# Try admin function: with an admin user
-# header = {'x-access-token': mock1_token}
-# response = requests.put(BASE + 'user/6666666', headers=header)
-# print('Success expected:')
-# print(response.json())
-# print('')
-#
-# # Try function (auth token required): NO auth token given
-# response = requests.put(BASE + 'cameras')
-# print("405 expected:")
-# print(response)
-# print('')
-#
-# # Try function (auth token required): auth token given
-# header = {'x-access-token': mock1_token}
-# response = requests.get(BASE + 'cameras', headers=header)
-# print("200 expected:")
-# print(response)
-# print('')
