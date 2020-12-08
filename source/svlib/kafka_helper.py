58243e19a4cc22286d2b183ab5b178f6e2304657
hkristof03 <hkristof03@hotmail.com>
2020-12-08 22:32:17 +0100

    Implements refactored version of kafka_helper
    Kafka helper is used by the Secure Vision applications
    to produce and consume message to/from Kafka.
    
    An Admin class is used for basic operations like topic
    creation and deletion.

--- source/svlib/kafka_helper.py
+++ source/svlib/kafka_helper.py
@@ -1,10 +1,11 @@
-from typing import List, Union, Callable
+from typing import List, Union, Tuple, Callable
 import traceback
 
 import json
 from confluent_kafka import Producer, Consumer, KafkaException
+from confluent_kafka.admin import AdminClient, NewTopic
 
-from svtools import SecureVisionTools as svt
+from svlib.svtools import svtools as svt
 
 log = svt.log
 
@@ -12,17 +13,19 @@ log = svt.log
 class KafkaHelper(object):
     def __init__(self, target_landscape='custom', config_section=None):
         self.kafka_config = {
-            'bootstrap.servers': svt.conf.get('kafka', 'bootstrap_servers')
+            'bootstrap.servers': svt.conf.get('kafka', 'bootstrap.servers')
         }
         self.producer = None
-        log.info("...")
+        log.info(
+            "Initialized new KafkaHelper object with config: "
+            f"{self.kafka_config}"
+        )
 
-    def publish(self, topic: str, message: Union[dict, str]) ->None:
-        """
+    def publish(self, topic: str, message: Union[dict, str]) -> None:
+        """Posts the passed message to the target Kafka topic.
 
-        :param topic:
-        :param message:
-        :return:
+        :param topic: Identifier of the target topic
+        :param message: Message in a dictionary or string format
         """
         assert isinstance(message, str) or isinstance(message, dict)
 
@@ -31,10 +34,10 @@ class KafkaHelper(object):
 
         if isinstance(message, dict):
             message = json.dumps(message)
-
         # Asynchronous message producing
         self.producer.produce(topic, message.encode('utf-8'))
-
+        self.producer.flush()
+        log.info(f"Posted a document to kafka topic: {topic}")
 
     def consume_forever(
         self,
@@ -53,29 +56,74 @@ class KafkaHelper(object):
         callbacks = dict(zip(topics, callback_functions))
         self.kafka_config.update({
             'group.id': group_id,
-            'session.timeout.ms': 5000,
             'auto.offset.reset': 'earliest'
         })
-        c = Consumer(self.kafka_config, logger=svt.log, debug='fetch')
+        c = Consumer(self.kafka_config)
         c.subscribe(topics)
         # Read messages
         try:
             while True:
-                message = c.poll(timeout=1.0)
-                if not message:
+                msg = c.poll(timeout=1.0)
+                if not msg:
                     log.info(
                         "There was no message on the subscribed Kafka topics!"
                     )
-                elif message.error():
-                    raise KafkaException(message.error())
+                elif msg.error():
+                    raise KafkaException(msg.error())
                 else:
-                    message = json.loads(message.value().decode('utf-8'))
-                    callbacks[message.topic()](message)
+                    message = json.loads(msg.value().decode('utf-8'))
+                    callbacks[msg.topic()](message)
 
         except Exception as error:
             log.error(
-                f"Unexpected event occured! Error: {traceback.format_exc()}"
+                f"Unexpected event occurred! Error: {traceback.format_exc()}"
             )
         finally:
             # Shut down the consumer to commit the current offsets
             c.close()
+
+
+class KafkaAdmin:
+    def __init__(self):
+        self.kafka_config = {
+            'bootstrap.servers': svt.conf.get('kafka', 'bootstrap_servers')
+        }
+        self.admin = AdminClient(self.kafka_config)
+
+    def create_topics(self, topics: List[Tuple[str, int, int]]) -> None:
+        """Creates a list of kafka topics.
+
+        :param topics: List of tuples where:
+            1. element: name of the topic to create
+            2. element: number of partitions
+            3. element: number of replicas in the cluster
+        """
+        producer = Producer(
+            {'bootstrap.servers': svt.conf.get('kafka', 'bootstrap_servers')}
+        )
+        existing_topics = producer.list_topics().topics
+        new_topics = [
+            (topic, 1, 1) for topic in topics if topic not in existing_topics
+        ]
+        nts = [NewTopic(top[0], top[1], top[2]) for top in topics]
+
+        if nts:
+            self.admin.create_topics(nts)
+            log.info(f"Created topics: {topics}")
+        else:
+            log.info(f"Topics: {topics} are already existed!")
+
+    def delete_topics(self, topics: List[str]) -> None:
+        """Deletes a list of topics.
+
+        :param topics: List of strings where strings indicate topics
+        """
+        self.admin.delete_topics(topics)
+
+
+
+
+
+
+
+
