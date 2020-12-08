33bbe57c3b149d911c40d720580ea051deb7b84a
hkristof03 <hkristof03@hotmail.com>
2020-12-08 22:22:56 +0100

    Adds initial version of threat detector to deploy
    First version of the threat detector built upon base_python3_ml

---
+++ deploy/threat_predictor/app_start.sh
@@ -0,0 +1,6 @@
+# set environment variables for the env the current script belongs to
+# the full path of this script
+APP_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $APP_PATH
+
+docker-compose start "$@"
