9ceee5a55af4b4d246c23e6085852f4a2536cabf
Balint Domian <bu1t3e@inf.elte.hu>
2020-12-15 11:44:20 +0100

    Clean db refactor

--- source/backend/database/clean_db.py
+++ source/backend/database/clean_db.py
@@ -2,6 +2,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from containers import Databases
 
-Base = declarative_base()
-Base.metadata.reflect(Databases.base_engine)
-Base.metadata.drop_all(Databases.base_engine)
+
+def clean_db():
+    Base = declarative_base()
+    Base.metadata.reflect(Databases.base_engine)
+    Base.metadata.drop_all(Databases.base_engine)
+
+
+if __name__ == "__main__":
+    clean_db()
