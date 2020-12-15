2020c22e1c0083d3dd403ea3c56677024e6ac37b
Balint Domian <bu1t3e@inf.elte.hu>
2020-12-15 11:58:46 +0100

    Check camera number and add accordingly

--- source/backend/database/create_db.py
+++ source/backend/database/create_db.py
@@ -12,7 +12,7 @@ from feedback import Feedback
 # Keep these imports
 
 
-def create_db():
+def create_db(num_of_cameras):
     Base.metadata.create_all(Databases.base_engine)
     # 2. Creates a session object that can be later used for adding elements and querying
     user_handler_instance = Handlers.user_handler()
@@ -21,14 +21,22 @@ def create_db():
     # As it is available only for the selected guards, random users can not register
     # During the database setup the user registration should be handled
     user_handler_instance.register_users_unique(os.path.join(os.getcwd(), 'db_json', "users.json"))
-    cam_handler_instance.add_camera()
-    cam_handler_instance.add_camera()
-    # TODO: DONT ADD CAMERA IF ALREADY EXISTS
-    cam_handler_instance.update_start_camera(1)
-    cam_handler_instance.update_start_camera(2)
+
+    cam_list = cam_handler_instance.all_cams()
+    if len(cam_list) == num_of_cameras:
+        for cam_instance in cam_list:
+            cam_handler_instance.update_start_camera(cam_instance.id)
+    else:
+        for i in range(num_of_cameras - len(cam_list)):
+            cam_handler_instance.add_camera()
+
+        cam_new = cam_handler_instance.all_cams()
+        for cam_instance in cam_new:
+            cam_handler_instance.update_start_camera(cam_instance.id)
+
     user_handler_instance.release_resources()
     cam_handler_instance.release_resources()
 
 
 if __name__ == "__main__":
-    create_db()
+    create_db(num_of_cameras=2)
